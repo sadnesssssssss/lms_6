94965cf2f6735d5cd1b8a2de8fe9a2e98580b1d7
askdjfhasdkjfh <77674821+sadnesssssssss@users.noreply.github.com>
2023-03-20 00:18:59 +0900

    HitBox Rework

--- Assets/Scripts/Scale.cs
+++ Assets/Scripts/Scale.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class Scale : MonoBehaviour
 {
 
+    private BoxCollider _collider;
+
+    private void Start()
+    {
+        _collider = gameObject.GetComponentInParent<BoxCollider>();
+    }
+
     public void SetScale(float value)
     {
+        _collider.size = Vector3.one * value;
         transform.localScale = Vector3.one * value;
     }
 
